21881222e3df0dd6a8661223fb64ef2016609c6c
Eric Alcaide <ericalcaide1@gmail.com>
2024-07-21 23:15:44 +0200

    fw?

--- fla/ops/delta_rule/recurrent_fuse_mine.py
+++ fla/ops/delta_rule/recurrent_fuse_mine.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2023, Yu Zhang, Songlin Yang
 
-from typing import Tuple
-
+import functools
 import torch
 import triton
 import triton.language as tl
-
-import functools
 # on-the-fly computation without materializing hidden statets into HBMs
 
 
@@ -27,6 +24,7 @@ def fused_recurrent_fwd_kernel(
     k,  # key [B, H, L, D_head_V]
     v,  # value [B, H, L, D_head_V].
     beta,  # beta [B, H, L]
+    alpha, # alpha [B, H, L] (potentially D_head_K)
     o,  # output [B, H, L, D_head_V]
     initial_state,
     final_state,  # final hidden state [B, H, D_head_K, D_head_V]
@@ -59,6 +57,7 @@ def fused_recurrent_fwd_kernel(
     p_k = k + i_bh * s_qk_h + i_k * BK + tl.arange(0, BK)
     p_v = v + i_bh * s_vo_h + i_v * BV + tl.arange(0, BV)
     p_beta = beta + i_bh * T
+    p_alpha = alpha + i_bh * T
     p_o = o + (i_bh + i_k * B * H) * s_vo_h + i_v * BV + tl.arange(0, BV)
 
     mask_bk = (i_k * BK + tl.arange(0, BK)) < DK
@@ -77,6 +76,10 @@ def fused_recurrent_fwd_kernel(
         _k = tl.load(p_k, mask=mask_bk, other=0).to(tl.float32)
         _v = tl.load(p_v, mask=mask_bv, other=0).to(tl.float32)
         _q = tl.load(p_q, mask=mask_bk, other=0).to(tl.float32) * scale
+
+        # FIXME: hypnopump@ this is the momentum version (to state and delta, harder gradient)
+        h *= tl.load(p_alpha).to(tl.float32)
+
         _v_minus = tl.sum(h * _k[None, :], axis=1)
         _v -= _v_minus
         _beta = tl.load(p_beta).to(tl.float32)
@@ -84,6 +87,10 @@ def fused_recurrent_fwd_kernel(
         tl.store(p_v, _v.to(p_v.dtype.element_ty), mask=mask_bv)
         _v *= _beta
         h += _k[None, :] * _v[:, None]
+
+        # FIXME: hypnopump@ this is the decayed version (only to state)
+        # h = h * alpha[None, None] + _k[None, :] * _v[:, None]
+
         _o = h * _q[None, :]
         _o = tl.sum(_o, axis=1)
         tl.store(p_o, _o.to(p_o.dtype.element_ty), mask=mask_bv)
@@ -110,12 +117,14 @@ def fused_recurrent_bwd_kernel(
     k,  # key [B, H, L, D_head_V]
     v,  # value [B, H, L, D_head_V]
     beta,  # beta [B, H, L]
+    alpha, # alpha [B, H, L]
 
     do,  # gradient of output [B, H, L, D_head_V]
     dq,  # gradient of query [NV, B, H, L, D_head_K]
     dk,  # gradient of key [NV, B, H, L, D_head_K]
     dv,  # gradient of value [NK, B, H, L, D_head_V]
     dbeta,  # gradient of beta [B, H, L]
+    dalpha,  # gradient of alpha [B, H, L]
 
     # initial hidden state initialization [B, H, D_head_K, D_head_V]
     initial_state,
@@ -148,6 +157,8 @@ def fused_recurrent_bwd_kernel(
     p_v = v + i_bh * s_vo_h + i_v * BV + tl.arange(0, BV) + (T - 1) * DV
     p_beta = beta + i_bh * T + T - 1
     p_dbeta = dbeta + (i_bh + i_v * B * H) * T + T - 1
+    p_alpha = alpha + i_bh * T + T - 1
+    p_dalpha = dalpha + (i_bh + i_v * B * H) * T + T - 1
 
     p_dk = dk + (i_bh + i_v * B * H) * s_qk_h + i_k * \
         BK + tl.arange(0, BK) + (T - 1) * DK
@@ -161,6 +172,8 @@ def fused_recurrent_bwd_kernel(
         _k = tl.load(p_k, mask=mask_bk, other=0).to(tl.float32)
         _v = tl.load(p_v, mask=mask_bv, other=0).to(tl.float32)
         _beta = tl.load(p_beta).to(tl.float32)
+        _alpha = tl.load(p_alpha).to(tl.float32)
+
         d_h += _q[:, None] * _do[None, :]
         d_k = tl.sum(d_h * _v[None, :] * _beta, axis=1)
         d_v = tl.sum(d_h * _k[:, None], axis=0)
@@ -168,11 +181,15 @@ def fused_recurrent_bwd_kernel(
         d_beta = tl.sum(d_v * _v)
         d_v = d_v * _beta
 
+        # TODO: store h or recompute H in this forward bc maybe it's (d_h * h).sum()
+        d_alpha = tl.sum(d_h)
+
         tl.store(p_dk, d_k.to(p_dk.dtype.element_ty), mask=mask_bk)
         tl.store(p_dv, d_v.to(p_dv.dtype.element_ty), mask=mask_bv)
         tl.store(p_dbeta, d_beta.to(p_dbeta.dtype.element_ty))
+        tl.store(p_dalpha, d_alpha.to(p_dalpha.dtype.element_ty))
 
-        d_h -= _k[:, None] * d_v[None, :]
+        d_h = d_h * alpha[None, None] - _k[:, None] * d_v[None, :]
 
         p_do -= DV
         p_q -= DK
@@ -191,6 +208,7 @@ def fused_recurrent_bwd_kernel(
     p_k = k + i_bh * s_qk_h + i_k * BK + tl.arange(0, BK)
     p_v = v + i_bh * s_vo_h + i_v * BV + tl.arange(0, BV)
     p_beta = beta + i_bh * T
+    p_alpha = alpha + i_bh * T
     p_do = do + i_bh * s_vo_h + i_v * BV + tl.arange(0, BV)
     p_dq = dq + (i_bh + i_v * B * H) * s_qk_h + i_k * BK + tl.arange(0, BK)
     p_dv = dv + (i_bh + i_k * B * H) * s_vo_h + i_v * BV + tl.arange(0, BV) + DV
@@ -208,9 +226,10 @@ def fused_recurrent_bwd_kernel(
         _v = tl.load(p_v, mask=mask_bv, other=0).to(tl.float32)
         _do = tl.load(p_do, mask=mask_bv, other=0).to(tl.float32)
         _beta = tl.load(p_beta).to(tl.float32)
+        _alpha = tl.load(p_alpha).to(tl.float32)
         _v *= _beta
 
-        h += _k[:, None] * _v[None, :]
+        h = h * _alpha[None, None] + _k[:, None] * _v[None, :]
         _d_q = h * _do[None, :]
         d_q = tl.sum(_d_q, axis=1) * scale
         tl.store(p_dq, d_q.to(p_dq.dtype.element_ty), mask=mask_bk)
@@ -228,13 +247,14 @@ def fused_recurrent_bwd_kernel(
         p_dv += DV
         p_dq += DK
         p_beta += 1
+        p_alpha += 1
 
 
 class FusedRecurrentFunction(torch.autograd.Function):
 
     @staticmethod
     @contiguous
-    def forward(ctx, q, k, v, beta, initial_state=None, output_final_state=False):
+    def forward(ctx, q, k, v, beta, alpha, initial_state=None, output_final_state=False):
         batch_size, n_heads, seq_len, d_head_qk = q.shape
         d_head_v = v.shape[-1]
 
@@ -253,7 +273,7 @@ class FusedRecurrentFunction(torch.autograd.Function):
 
         grid = (NV, NK, batch_size * n_heads)
         fused_recurrent_fwd_kernel[grid](
-            q, k, v, beta, o, initial_state, final_state,
+            q, k, v, beta, alpha, o, initial_state, final_state,
             q.stride(1), q.stride(2), q.stride(3),
             v.stride(1), v.stride(2), v.stride(3),
             batch_size, n_heads, seq_len, scale,
@@ -264,13 +284,13 @@ class FusedRecurrentFunction(torch.autograd.Function):
             STORE_FINAL_STATE=final_state is not None
         )
         o = o.sum(0)
-        ctx.save_for_backward(q, k, v, beta, initial_state)
+        ctx.save_for_backward(q, k, v, beta, alpha, initial_state)
         return o, final_state
 
     @staticmethod
     @contiguous
     def backward(ctx, do, d_final_state=None):
-        q, k, v, beta, initial_state = ctx.saved_tensors
+        q, k, v, beta, alpha, initial_state = ctx.saved_tensors
         batch_size, n_heads, seq_len, d_head_qk = q.shape
         d_head_v = v.shape[-1]
         scale = d_head_qk ** -0.5
@@ -285,9 +305,10 @@ class FusedRecurrentFunction(torch.autograd.Function):
         dv = q.new_empty(NK, batch_size, n_heads, seq_len, d_head_v)
         grid = (NV, NK, batch_size * n_heads)
         dbeta = q.new_empty(NV, batch_size, n_heads, seq_len)
+        dalpha = q.new_empty(NV, batch_size, n_heads, seq_len)
 
         fused_recurrent_bwd_kernel[grid](
-            q, k, v, beta, do, dq, dk, dv, dbeta, initial_state,
+            q, k, v, beta, alpha, do, dq, dk, dv, dbeta, dalpha, initial_state,
             q.stride(1), q.stride(2), q.stride(3),
             v.stride(1), v.stride(2), v.stride(3),
             batch_size, n_heads, seq_len, scale,
@@ -300,7 +321,8 @@ class FusedRecurrentFunction(torch.autograd.Function):
         dk = dk.sum(0)
         dv = dv.sum(0)
         dbeta = dbeta.sum(0)
-        return dq.to(q), dk.to(k), dv.to(v), dbeta.to(beta), None, None
+        dalpha = dalpha.sum(0)
+        return dq.to(q), dk.to(k), dv.to(v), dbeta.to(beta), dalpha.to(alpha), None, None
 
 
 def fused_recurrent_linear_attn_delta_rule(
@@ -308,15 +330,18 @@ def fused_recurrent_linear_attn_delta_rule(
     k: torch.Tensor,
     v: torch.Tensor,
     beta: torch.Tensor = None,
+    alpha: torch.Tensor = None,
     initial_state: torch.Tensor = None,
     output_final_state: bool = False,
     normalize: bool = False
-) -> Tuple[torch.Tensor, torch.Tensor]:
+) -> tuple[torch.Tensor, torch.Tensor]:
     if initial_state is not None:
         initial_state = initial_state.detach()
     if beta is None:
         beta = torch.ones_like(q[..., 0])
-    o, final_state = FusedRecurrentFunction.apply(q, k, v, beta, initial_state, output_final_state)
+    if alpha is None:
+        alpha = torch.ones_like(q[..., 0])
+    o, final_state = FusedRecurrentFunction.apply(q, k, v, beta, alpha, initial_state, output_final_state)
     return o, final_state
 
 
@@ -373,10 +398,26 @@ if __name__ == "__main__":
     beta.requires_grad = True
     state.requires_grad = True
 
+
+    ### compute torch with gradients
+    o, s = abakava_fw(q, k, v, w, beta)
+    o.retain_grad()
+    l = loss(o)
+    l.backward(retain_graph=True)
+
+    do = o.grad
+
     ### compute triton
     # fw:
     with th.no_grad():
-        o_triton = fused_recurrent_linear_attn_delta_rule(q, k, v, beta)
+        o_triton = fused_recurrent_linear_attn_delta_rule(q, k, v, beta, w)
 
+    print(o - o_triton)
     breakpoint()
+
+
+
+
+
+
 
